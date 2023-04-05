60e1f5cc3b34cbbecd18be9566b1c8c063e9e1d4
Muneeb Arshad <42831583+mnbrshd@users.noreply.github.com>
2023-04-05 12:17:02 +0500

    Fixed indents and dna2

--- train.py
+++ train.py
@@ -19,36 +19,36 @@ class Route():
             self.dna.append(rnd)
         self.dna.append(0)
 
-        #Building the Crossover method
-        def mix(self, dna1, dna2):
-            self.dna = dna1.copy()
+    #Building the Crossover method
+    def mix(self, dna1, dna2):
+        self.dna = dna1.copy()
 
-            for i in range(self.dnaLength-1):
-                if np.random.rand() <= 0.5:
-                    previous = self.dna[i]
-                    inx = self.dna.index(dna2[i])
-                    self.dna[inx] = previous
-                    self.dna[i] = dna2[i]
+        for i in range(self.dnaLength-1):
+            if np.random.rand() <= 0.5:
+                previous = self.dna[i]
+                inx = self.dna.index(dna2[i])
+                self.dna[inx] = previous
+                self.dna[i] = dna2[i]
 
-            #Random partial mutations 1
-            for i in range(self.dnaLength-1):
-                if np.random.rand() <= 0.1:
-                    previous = self.dna[i]
-                    rnd = np.random.randint(1, self.dnaLength)
-                    inx = self.dna.index(rnd)
-                    self.dna[inx] = previous
-                    self.dna[i] = rnd
+        #Random partial mutations 1
+        for i in range(self.dnaLength-1):
+            if np.random.rand() <= 0.1:
+                previous = self.dna[i]
+                rnd = np.random.randint(1, self.dnaLength)
+                inx = self.dna.index(rnd)
+                self.dna[inx] = previous
+                self.dna[i] = rnd
 
-                #Random partial mutation 2
-                elif np.random.rand() <= 0.1:
-                    rnd = np.random.randint(1, self.dnaLength)
-                    prevInx = self.dna.index(rnd)
-                    self.dna.insert(i, rnd)
+            #Random partial mutation 2
+            elif np.random.rand() <= 0.1:
+                rnd = np.random.randint(1, self.dnaLength)
+                prevInx = self.dna.index(rnd)
+                self.dna.insert(i, rnd)
 
-                    if i>=prevInx:
-                        self.dna.pop(prevInx)
-                    else:
-                        self.dna.pop(prevInx+1)
+                if i>=prevInx:
+                    self.dna.pop(prevInx)
+                else:
+                    self.dna.pop(prevInx+1)
 
 #Initializing the main code
 populationSize = 50
@@ -105,7 +105,7 @@ while True:
                 inx2 = np.random.randint(0, nSelected)
             
             dna1 = sortedPop[inx1].dna
-            dna1 = sortedPop[inx2].dna
+            dna2 = sortedPop[inx2].dna
 
             newRoute.mix(dna1, dna2)
             population.append(newRoute)
